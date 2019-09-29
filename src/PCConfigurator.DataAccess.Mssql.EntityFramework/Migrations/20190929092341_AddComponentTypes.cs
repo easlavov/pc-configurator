a2b8faa170b0c27cdb41e4ca219cd78964483a00
Emil Slavov <easlavov@gmail.com>
2019-09-29 12:26:18 +0300

    Added EF Core mappings for component types. Added migration for component types table. Seeded basic component types.

---
+++ src/PCConfigurator.DataAccess.Mssql.EntityFramework/Migrations/20190929092341_AddComponentTypes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PCConfigurator.DataAccess.Mssql.EntityFramework.Migrations
+{
+    public partial class AddComponentTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "component_types",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_component_types", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "component_types",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1L, "Motherboard" },
+                    { 2L, "CPU" },
+                    { 3L, "GPU" },
+                    { 4L, "RAM" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "component_types");
+        }
+    }
+}
---
+++ src/PCConfigurator.DataAccess.Mssql.EntityFramework/PCConfiguratorDBContext.cs
@@ -0,0 +1,37 @@
+﻿namespace PCConfigurator.DataAccess.Mssql.EntityFramework
+{
+    using System;
+
+    using PCConfigurator.Core;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class PCConfiguratorDBContext : DbContext
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=PCConfigurator;Trusted_Connection=True;";
+
+        public DbSet<ComponentType> ComponentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ComponentType>().ToTable("component_types");
+            modelBuilder.Entity<ComponentType>()
+                .Property(t => t.Id).HasColumnName("id").UseIdentityColumn();
+            modelBuilder.Entity<ComponentType>()
+                .Property(t => t.Name).HasColumnName("name").IsRequired();
+            modelBuilder.Entity<ComponentType>()
+                .HasKey(t => t.Id);
+
+            modelBuilder.Entity<ComponentType>().HasData(
+                    new ComponentType(1, "Motherboard"),
+                    new ComponentType(2, "CPU"),
+                    new ComponentType(3, "GPU"),
+                    new ComponentType(4, "RAM")
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(ConnectionString);
+
+    }
+}
