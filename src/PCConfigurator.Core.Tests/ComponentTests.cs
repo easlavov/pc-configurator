743af2f314f2d044dbc738268dcaf39ac2ec20a4
Emil Slavov <easlavov@gmail.com>
2019-09-29 20:19:46 +0300

    Added Component class.

---
+++ src/PCConfigurator.Core.Tests/ComponentTests.cs
@@ -0,0 +1,45 @@
+﻿namespace PCConfigurator.Core.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class ComponentTests
+    {
+        [Test]
+        public void Constructor_ThrowsOnNullComponentType()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new Component(1, null, "GTX 960", 100);
+            });
+        }
+
+        [Test]
+        public void Constructor_ThrowsOnNullName()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new Component(1, new ComponentType(1, "GPU"), null, 100);
+            });
+        }
+
+        [Test]
+        public void Constructor_ThrowsOnEmptyName()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new Component(1, new ComponentType(1, "GPU"), string.Empty, 100);
+            });
+        }
+
+        [Test]
+        public void Constructor_ThrowsOnNegativePrice()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                new Component(1, new ComponentType(1, "GPU"), "GTX 960", -100);
+            });
+        }
+    }
+}
--- src/PCConfigurator.Core/Component.cs
+++ src/PCConfigurator.Core/Component.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PCConfigurator.Core
+﻿namespace PCConfigurator.Core
 {
-    class Component
+    using System;
+
+    public class Component : Entity
     {
+        public Component(long id, ComponentType componentType, string name, decimal price) : 
+            base(id)
+        {
+            if (componentType == null)
+                throw new ArgumentNullException(nameof(componentType));
+
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            if (price < 0)
+                throw new ArgumentOutOfRangeException($"{nameof(price)} must be a non-negative value!");
+
+            ComponentType = componentType;
+            Name = name;
+            Price = price;
+        }
+
+        public ComponentType ComponentType { get; }
+
+        public string Name { get; }
+
+        public decimal Price { get; }
     }
 }
