d1b5d16c34aeeb5122e6ae2769e572cebbbd213f
Emil Slavov <easlavov@gmail.com>
2019-10-01 12:24:51 +0300

    Fixed broken delete button.

--- src/PCConfigurator.Web/wwwroot/js/grid.js
+++ src/PCConfigurator.Web/wwwroot/js/grid.js
@@ -13,7 +13,7 @@
                     title: "Actions",
                     render: function (data, type, row) {
                         var editButton = `<button class="btn btn-primary edit-entity" data-id=${data.id}>Edit</button>`;
-                        var deleteButton = `<button class="btn btn-danger elete-entity" data-id=${data.id}>Delete</button>`;
+                        var deleteButton = `<button class="btn btn-danger delete-entity" data-id=${data.id}>Delete</button>`;
                         return editButton + deleteButton;
                     }
                 },
